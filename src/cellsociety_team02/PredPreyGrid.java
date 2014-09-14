606c625f1263c248d1cd726f584d2e2d4b8c2046
gregory-lyons <gmlyons14@gmail.com>
2014-09-14 16:44:14 -0400

    Updated Grid and Cell class

---
+++ src/cellsociety_team02/PredPreyGrid.java
@@ -0,0 +1,16 @@
+package cellsociety_team02;
+
+import java.util.HashMap;
+
+public class PredPreyGrid extends Grid {
+
+	public PredPreyGrid(HashMap<String, String> parametersMap, int[][] initialStates) {
+		super(parametersMap, initialStates);
+	}
+
+	@Override
+	protected void updateCell(int i, int j) {
+		
+	}
+
+}
