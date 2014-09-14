636b75a9985ceeb01d2c7be6814d9f6de2cc5146
gregory-lyons <gmlyons14@gmail.com>
2014-09-14 17:18:46 -0400

    Implemented main class working with XML input and Grid creation

--- src/cellsociety_team02/Cell.java
+++ src/cellsociety_team02/Cell.java
@@ -4,17 +4,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Cell extends Rectangle {
-	public Cell(double x, double y, int state) {
-		super(x,y);
+	public Cell(double x, double y, double w, double h, int state) {
+		super(x,y, w, h);
 		switch (state) {
-			case 0: setFill(Color.WHITE);
-			case 1: setFill(Color.BLACK);
-			case 2: setFill(Color.RED);
-			case 3: setFill(Color.ORANGE);
-			case 4: setFill(Color.YELLOW);
-			case 5: setFill(Color.GREEN);
-			case 6: setFill(Color.BLUE);
-			case 7: setFill(Color.VIOLET);		
+			case 0: setFill(Color.WHITE); break;
+			case 1: setFill(Color.BLACK); break;
+			case 2: setFill(Color.RED); break;
+			case 3: setFill(Color.ORANGE); break;
+			case 4: setFill(Color.YELLOW); break;
+			case 5: setFill(Color.GREEN); break;
+			case 6: setFill(Color.BLUE); break;
+			case 7: setFill(Color.VIOLET);	break;
 		}
 	}
 }
--- src/cellsociety_team02/MainTest.java
+++ src/cellsociety_team02/MainTest.java
@@ -3,11 +3,17 @@ package cellsociety_team02;
 import java.io.File;
 import java.util.Map;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainTest extends Application{
+	
+	Grid myGrid;
+	
 	@Override
 	public void start (Stage s)
 	{
@@ -19,6 +25,22 @@ public class MainTest extends Application{
 		Map<String,String> parameters = xml.makeParameterMap();
 		int[][] array = xml.makeArray();
 		xml.printArray();
+		
+		
+		s.setTitle("CA Simulation");
+		switch(model)  {
+		case "Fire": myGrid = new FireGrid(parameters, array); break;
+		}
+		Scene scene = myGrid.init(600, 700);
+		s.setScene(scene);
+		s.show();
+		
+		// sets the game's loop 
+		KeyFrame frame = myGrid.startHandlers();
+		Timeline animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
 	}
 
 	/**
