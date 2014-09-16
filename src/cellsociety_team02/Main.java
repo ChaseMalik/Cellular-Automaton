67778e7d8d79bd1d8e7358626fa7e0381fc74c30
gregory-lyons <gmlyons14@gmail.com>
2014-09-16 19:45:21 -0400

    Added loadSimulation method, reconfigured main

--- src/cellsociety_team02/Main.java
+++ src/cellsociety_team02/Main.java
@@ -13,52 +13,19 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-	
+
 	private Grid myGrid;
 	private KeyFrame frame;
 	private double speed;
 	private Timeline animation;
-	
+	private Stage myStage;
+	private Scene myScene;
+
 	@Override
 	public void start (Stage s)
 	{
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Choose XML Source File");
-		File file = fileChooser.showOpenDialog(s);
-		XMLParser xml = new XMLParser(file);
-		String model = xml.getModel();
-		Map<String,String> parameters = xml.makeParameterMap();
-		int[][] array = xml.makeArray();
-		animation = new Timeline();
-		xml.printArray();
-		
-		
-		s.setTitle("CA Simulation");
-		switch(model)  {
-		case "Fire": myGrid = new FireGrid(parameters, array); break;
-		case "PredPrey" : myGrid = new PredPreyGrid(parameters, array); break;
-		case "Segregation": myGrid = new SegregationGrid(parameters,array); break;
-
-		}
-		Scene scene = myGrid.init(600, 700);
-		s.setScene(scene);
-		s.show();
-		speed = 1.0;
-		
-		scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
-			@Override public void handle(KeyEvent ke) {
-				switch (ke.getCode()){
-				case UP: speed*=0.5; startAnimation(); System.out.println("Up accessed");break;
-				case DOWN: speed*=2; startAnimation(); System.out.println("Down accessed");break;
-				case SPACE: myGrid.startStop(); break;
-				case RIGHT: myGrid.step(); break;
-				default:
-					break;
-				}
-			}			
-		});
-		
-		startAnimation();
+		myStage = s;
+		loadSimulation(s);
 	}
 
 	/**
@@ -68,7 +35,7 @@ public class Main extends Application{
 	{
 		launch(args);
 	}
-	
+
 	private void startAnimation() {
 		animation.stop();
 		frame = myGrid.startHandlers(speed);
@@ -76,6 +43,46 @@ public class Main extends Application{
 		animation.getKeyFrames().clear();
 		animation.getKeyFrames().add(frame);
 		animation.play();
+	}
+
+	private void loadSimulation(Stage s){
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Choose XML Source File");
+		File file = fileChooser.showOpenDialog(s);
+		XMLParser xml = new XMLParser(file);
+		String model = xml.getModel();
+		Map<String,String> parameters = xml.makeParameterMap();
+		int[][] array = xml.makeArray();
+		animation = new Timeline();
+		xml.printArray();
+		s.setTitle("CA Simulation");
+		switch(model)  {
+		case "Fire": myGrid = new FireGrid(parameters, array); break;
+		case "PredPrey" : myGrid = new PredPreyGrid(parameters, array); break;
+		case "Segregation": myGrid = new SegregationGrid(parameters,array); break;
+		}
+		myScene = myGrid.init(600, 700);
+		s.setScene(myScene);
+		s.show();
+		speed = 1.0;
+		startAnimation();
+		makeKeyHandler();
+	}
+	
+	private void makeKeyHandler(){
+		myScene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+			@Override public void handle(KeyEvent ke) {
+				switch (ke.getCode()){
+				case UP: speed*=0.5; startAnimation(); System.out.println("Up accessed");break;
+				case DOWN: speed*=2; startAnimation(); System.out.println("Down accessed");break;
+				case SPACE: myGrid.startStop(); break;
+				case RIGHT: myGrid.step(); break;
+				case L: myStage.close(); loadSimulation(myStage); break;
+				default:
+					break;
+				}
+			}			
+		});
 	}
 	
 }
